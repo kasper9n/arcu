cbd9cc60b5f123836d0c8eb6b8d6eee8ea552db7
Kasper <kasperkh.kh@gmail.com>
2021-04-26 05:26:19 +0200

    Update dependencies

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,7 +3,7 @@
   windows_subsystem = "windows"
 )]
 
-use tauri::{generate_context, WindowUrl};
+use tauri::{generate_context, Attributes, WindowUrl};
 
 mod cmd;
 
@@ -12,19 +12,18 @@ fn main() {
   ctx.config.tauri.windows = Vec::new();
   tauri::Builder::default()
     .invoke_handler(tauri::generate_handler![cmd::query])
-    .create_window("main".to_owned(), WindowUrl::default(), |mut webview| {
-      webview.resizable = true;
-      webview.title = "Arcu".to_string();
-      // webview.transparent = false;
-      webview.decorations = false;
-      webview.always_on_top = false;
-      webview.width = 800.0;
-      webview.height = 600.0;
-      webview.min_width = Some(300.0);
-      webview.min_height = Some(150.0);
-      webview.fullscreen = false;
-      webview.skip_taskbar = false;
-      return webview;
+    .create_window("main".into(), WindowUrl::default(), |webview| {
+      webview
+        .title("Arcu")
+        .resizable(true)
+        .transparent(false)
+        .decorations(false)
+        .always_on_top(false)
+        .width(800.0)
+        .height(600.0)
+        .min_width(300.0)
+        .min_height(150.0)
+        .fullscreen(false)
     })
     .run(ctx)
     .expect("error while running tauri app");
